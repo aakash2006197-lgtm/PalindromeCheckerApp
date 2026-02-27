1226d753507660285dad496555a4c27142cf0d7f
Aakash07 <aakash2006197@gmail.com>
2026-02-27 09:11:10 +0530

    Add files via upload

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,27 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "noon";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
 
+        input = input.replaceAll("\\s+", "").toLowerCase();
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome : " + isPalindrome);
+        if (isPalindrome) {
+            System.out.println(input + " is Palindrome : true");
+        } else {
+            System.out.println(input + " is Palindrome : false");
+        }
+
+        scanner.close();
     }
 }

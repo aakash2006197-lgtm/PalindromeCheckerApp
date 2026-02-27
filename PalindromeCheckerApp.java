9c3427d5412c8d5e547c01e73107e63a01ab7224
Aakash07 <aakash2006197@gmail.com>
2026-02-27 09:38:01 +0530

    Add files via upload

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,38 +1,80 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    public static boolean isPalindrome(Node head) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        if (head == null || head.next == null)
+            return true;
 
-        input = input.replaceAll("\\s+", "").toLowerCase();
 
-        Deque<Character> deque = new ArrayDeque<>();
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node current = slow;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static Node createLinkedList(String input) {
+        Node head = null, tail = null;
 
         for (char c : input.toCharArray()) {
-            deque.add(c);
-        }
+            Node newNode = new Node(c);
 
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
+        return head;
+    }
 
-        if (isPalindrome) {
-            System.out.println(input + " is Palindrome : true");
-        } else {
-            System.out.println(input + " is Palindrome : false");
-        }
+    public static void main(String[] args) {
 
-        scanner.close();
+        String input = "level";
+
+        Node head = createLinkedList(input);
+
+        boolean result = isPalindrome(head);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
     }
 }
